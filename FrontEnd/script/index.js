ca618fd6a8ce44bf12d92b262816e4e477870f97
Fany31-dev <s.rambeault@gmx.fr>
2026-03-02 11:39:03 +0100

    ajout fetch pour appel à l'API

---
+++ FrontEnd/script/index.js
@@ -0,0 +1,15 @@
+async function getData() {
+  const url = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getData ();
