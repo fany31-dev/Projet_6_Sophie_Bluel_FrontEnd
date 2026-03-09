da9f1b2fc3e2091c4267742a0c59b6b73af25ac6
Fany31-dev <s.rambeault@gmx.fr>
2026-03-09 11:12:30 +0100

    ajout fonction connexionUtilisateur page login

---
+++ FrontEnd/script/login.js
@@ -0,0 +1,38 @@
+const formulaireLogin = document.getElementById("login_form");
+
+formulaireLogin.addEventListener("submit", connexionUtilisateur);
+
+/* fonction connexion login utilisateur */
+async function connexionUtilisateur (event) {
+  event.preventDefault(); // bloque le comportement du navigateur
+
+  const userEmail = document.getElementById("login_email").value;
+  const password = document.getElementById("login_password").value;
+
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: userEmail, password }),
+    });
+
+    const resultat = await response.json();
+
+    if(response.ok) {
+    console.log("Vous etes connectés !!")
+    console.log(resultat);
+
+  } else {
+    alert("Erreur dans l’identifiant ou le mot de passe");
+  }
+
+  } catch (error) {
+    console.error("Erreur API :", error);
+  }
+  
+};
+
+
+
+
+
