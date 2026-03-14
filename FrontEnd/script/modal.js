7440dc00b1ae8f60aac799ff56cf1764070f6514
Fany31-dev <s.rambeault@gmx.fr>
2026-03-14 20:16:53 +0100

    creation de la fonction "closeModale" fermeture de la modale au click sur l'icone croix + mise en forme de la modale

--- FrontEnd/script/modal.js
+++ FrontEnd/script/modal.js
@@ -1,15 +1,38 @@
 
+let modal = null
+
+/***************ouverture de la modale *****************/
 function openModal (event) {
     event.preventDefault ();
-
     const selector = event.currentTarget.getAttribute("href");
     const target = document.querySelector(selector);
-
-    target.style.display ="inline-flex";
+    target.style.display =null;
     target.removeAttribute("aria-hidden");
     target.setAttribute("aria-modal", "true");
+    modal = target
+    modal.addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
 }
-    // console.log(document.getElementsByClassName("js-modal"))
+
+/***************fermeture de la modale *****************/
+function closeModal (event) {
+  if (modal === null) return
+    event.preventDefault ();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+    modal = null
+}
+
+function stopPropagation(e) {
+  e.stopPropagation ()
+}
+
+// console.log(document.getElementsByClassName("js-modal"))
 
     document.addEventListener("DOMContentLoaded", () => {
     const buttonModal = document.querySelectorAll(".js-modal");
