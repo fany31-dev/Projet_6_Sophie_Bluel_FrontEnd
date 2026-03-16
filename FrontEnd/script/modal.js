9049c98036f7b4e4c4738b033fdee32a44f3268a
Fany31-dev <s.rambeault@gmx.fr>
2026-03-16 15:04:39 +0100

    ajout switch modal1 vers modal2 au clic button ajouter

--- FrontEnd/script/modal.js
+++ FrontEnd/script/modal.js
@@ -34,7 +34,7 @@ function openModal (event) {
     event.preventDefault ();
     const selector = event.currentTarget.getAttribute("href");
     const target = document.querySelector(selector);
-    target.style.display =null;
+    target.style.display = null;
     target.removeAttribute("aria-hidden");
     target.setAttribute("aria-modal", "true");
     modal = target
@@ -80,7 +80,7 @@ function focusInModal(e) {
       index++;
     }
     if (index >= focusables.length) {
-      index = 0;
+      index = 0;      
     }
     if (index < 0) {
       index = focusables.length -1;
@@ -106,4 +106,24 @@ window.addEventListener("keydown", function (e) {
   if (e.key === "Shift" && modal !== null) {
     focusInModal(e)
   }
+});
+
+/*****************ouverture de la modale 1 vers la modale 2 */
+const modal1 = document.getElementById("modal1");
+const modal2 = document.getElementById("modal2");
+const openModal2 = document.getElementById("openModal2")
+
+// Bouton dans la modale 1 pour ouvrir la modale 2
+document.getElementById("openModal2").addEventListener("click", () => {
+  modal1.style.display = "none";
+  modal2.style.display = null;
+});
+
+document.querySelector("#modal2 .js-modal-close").addEventListener("click", () => {
+modal2.style.display = "none";
+});
+
+document.querySelector("#modal2 .js-modal-back").addEventListener("click", () => {
+modal2.style.display = "none";
+modal1.style.display = null; // retour à la modale 1
 });
